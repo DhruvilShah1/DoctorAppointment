ffa8796515607ffe410050c105c686254c83937d
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-05-18 12:08:37 +0530

    refactor: update socket initialization to use io instance and improve code formatting

--- BackendServer/socket/FrontendSocketConnection.js
+++ BackendServer/socket/FrontendSocketConnection.js
@@ -1,3 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://doctorappointment-lj0a.onrender.com/");
+export const socket = io("https://doctorappointment-lj0a.onrender.com", {
+  withCredentials: true,
+});
--- BackendServer/socket/socket.js
+++ BackendServer/socket/socket.js
@@ -1,16 +1,13 @@
-import { Server } from "socket.io";
+let io;
 
-let io = null;
-
-export const initSocket = (server) => {
-  io = new Server(server, {
-    cors: {
-      origin: "*",
-      credentials: true,
-    },
-  });
-
-  console.log("✅ Socket initialized");
+export const initSocket = (socketIo) => {
+  io = socketIo;
 };
 
-export const getIO = () => io;
+export const getIO = () => {
+  if (!io) {
+    throw new Error("Socket not initialized");
+  }
+
+  return io;
+};
