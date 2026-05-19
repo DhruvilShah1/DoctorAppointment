e4a945daba4cc6dc87cba3a9339e3c2bb45a274f
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-05-19 11:25:27 +0530

    fix: update cloudinary configuration to use hardcoded values

--- BackendServer/Config/cloudinary.js
+++ BackendServer/Config/cloudinary.js
@@ -1,9 +1,10 @@
-import { v2 as cloudinary } from "cloudinary";
+const { v2: cloudinary } = require("cloudinary");
+require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name:"dkpq0yqw4" ,
+  api_key:"136983381167343" ,
+  api_secret: "RjKP2OS65rl-hovSzWHV3B6nD18",
 });
 
 export default cloudinary;
