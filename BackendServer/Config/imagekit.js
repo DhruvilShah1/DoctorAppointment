02e3156b041c6d78e4107f59144ce6c15085c19c
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-05-19 11:37:41 +0530

    feat: replace Cloudinary with ImageKit for image handling

--- BackendServer/Config/imagekit.js
+++ BackendServer/Config/imagekit.js
@@ -1,10 +1,13 @@
-const { v2: cloudinary } = require("cloudinary");
-require("dotenv").config();
+import ImageKit from "imagekit";
 
-cloudinary.config({
-  cloud_name:"dkpq0yqw4" ,
-  api_key:"136983381167343" ,
-  api_secret: "RjKP2OS65rl-hovSzWHV3B6nD18",
+const imagekit = new ImageKit({
+  publicKey:
+    "public_zGZBg7/d9ls/iTlRog0qh++OSbc=",
+
+  privateKey:
+    "private_NWIoRU4rnx0LDV5FGT8bDad/4MA=",
+
+  urlEndpoint:"https://ik.imagekit.io/xqfepk1ftj"
 });
 
-module.exports = cloudinary;
+export default imagekit;
