51bcd280340bdd247b4fef7d2a36496ec72bee62
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-07-25 18:18:19 +0530

    fix: correct BASE_URL formatting and update sessionId to use dynamic user name

--- FrontEnd/src/LoginRegister/User/Chatbot.jsx
+++ FrontEnd/src/LoginRegister/User/Chatbot.jsx
@@ -92,7 +92,7 @@ const Chatbot = () => {
     return result;
   };
 
-  const BASE_URL = "https://doctorappointment-1-wwg3.onrender.com" | "http://127.0.0.1:5000"
+  const BASE_URL = "https://doctorappointment-1-wwg3.onrender.com/" | "http://127.0.0.1:5000"
 
   const sendMessage = async () => {
     if (!input.trim()) return;
@@ -126,10 +126,8 @@ const Chatbot = () => {
           },
 
           body: JSON.stringify({
-            sessionId: "Dhruvil",
-
+            sessionId: user.name,
             patient_id:user.id ,
-
             message: userText,
           }),
         },
