3e52979d80027895304216214851cbd55038f7f8
DhruvilShah1 <dhruvilshah3383@gmail.com>
2026-08-21 00:04:11 +0530

    fix: update EmailWorker transport configuration to use secure port 465

--- BackendServer/Worker/EmailWorker.js
+++ BackendServer/Worker/EmailWorker.js
@@ -7,7 +7,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
+    port: 465,
+    secure: true,
     family: 4,
     auth: {
         user: process.env.SMTP_USER,
